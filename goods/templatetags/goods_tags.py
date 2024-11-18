4a414ad95de071deb52e000f4ba29c075ebdd2cc
Mayzer <Tolik.Korzunov@yandex.ru>
2024-11-18 17:11:21 +0300

    Simple search created

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -14,9 +14,3 @@ def change_params(context, **kwargs):
     query = context['request'].GET.dict()
     query.update(kwargs)
     return urlencode(query)
-
-@register.simple_tag(takes_context=True)
-def change_params(context, **kwargs):
-    query = context['request'].GET.dict()
-    query.update(kwargs)
-    return urlencode(query)
