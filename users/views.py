e84f9a26d50d60290e04ce59e4eed575591a280e
Mayzer <Tolik.Korzunov@yandex.ru>
2024-11-30 16:48:35 +0300

    login required decorator - redirect to target page

--- users/views.py
+++ users/views.py
@@ -16,7 +16,11 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
-                return HttpResponseRedirect(reverse('main:index'))
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+            
+                return HttpResponseRedirect(reverse('user:profile'))
     else:
         form = UserLoginForm()
 
