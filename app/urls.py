3138b5defa6252f06c729e5c7b666580a55293b3
Mayzer <Tolik.Korzunov@yandex.ru>
2024-12-26 16:38:31 +0300

    Ready to deploy

--- app/urls.py
+++ app/urls.py
@@ -30,9 +30,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += [
-        path("__debug__/", include("debug_toolbar.urls")),    
-    ]
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # подключение MEDIA (фотографий) файлов к url адресу
 
 
