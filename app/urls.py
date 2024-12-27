ce7d3d2df5ab1cef01f40a18d3a13347a662c53a
Mayzer <Tolik.Korzunov@yandex.ru>
2024-12-27 15:45:53 +0300

    Changed styles

--- app/urls.py
+++ app/urls.py
@@ -30,6 +30,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),    
+    ]
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # подключение MEDIA (фотографий) файлов к url адресу
 
 
