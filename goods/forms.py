396490812adeaac1f147b70535c06f24cff57e4a
Mayzer <Tolik.Korzunov@yandex.ru>
2025-03-18 18:14:56 +0300

    Added styles on send review block

--- goods/forms.py
+++ goods/forms.py
@@ -4,13 +4,26 @@ from django.core.exceptions import ValidationError
 from django.contrib import messages
 
 class ReviewForm(forms.ModelForm):
+    RATING_CHOICES = [
+        (1, '★☆☆☆☆ Ужасно'),
+        (2, '★★☆☆☆ Плохо'),
+        (3, '★★★☆☆ Нормально'),
+        (4, '★★★★☆ Хорошо'),
+        (5, '★★★★★ Отлично'),
+    ]
+
+    rating = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.RadioSelect(attrs={'class': 'rating-radio'})
+    )
+
     class Meta:
         model = Review
         fields = ['rating', 'comment', 'image_1', 'image_2', 'image_3']
         widgets = {
-            'rating': forms.NumberInput(attrs={'min': 1, 'max': 5, 'class': 'form-control'}),
-            'comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Ваш отзыв поможет другим пользователям сделать выбор!'}),
         }
+
 
     def clean_comment(self):
         comment = self.cleaned_data.get('comment', '').strip()
